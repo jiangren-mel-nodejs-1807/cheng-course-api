b7aa21057e08f7b71e81722095e76bddd28bdf2d
Cheng Chang <cheng.chang@team.telstra.com>
2018-09-10 14:46:11 +1000

    return 400 if an enrollment exists

--- server/models/enrollment.js
+++ server/models/enrollment.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports = function(Enrollment) {
-
+    Enrollment.beforeRemote('create', function(context, user, next) {
+        Enrollment.findOne({
+            where: {
+                courseId: context.req.body.courseId,
+                studentId: context.req.body.studentId
+            }
+        }, function(err, results) {
+            if (err || results) {
+                context.res.status(400).send("enrollment existed");
+            } else {
+                next();
+            }
+        })
+    })
 };
